b3bb9036eb418683659df5a0c0bc1641fbaf2ebf
AnakinRibeiro <eduardor.contato@outlook.com>
2024-01-06 00:39:59 -0300

    Finished Menu component & tests

--- src/components/Menu/test.tsx
+++ src/components/Menu/test.tsx
@@ -1,4 +1,4 @@
-import { screen } from '@testing-library/react'
+import { fireEvent, screen } from '@testing-library/react'
 
 import Menu from '.'
 import { renderWithTheme } from 'utils/tests/helpers'
@@ -11,5 +11,44 @@ describe('<Menu />', () => {
     expect(screen.getByRole('img', { name: /won games/i })).toBeInTheDocument()
     expect(screen.getByLabelText(/search/i)).toBeInTheDocument()
     expect(screen.getByLabelText(/open shopping cart/i)).toBeInTheDocument()
+  })
+
+  it('should handle the open/close mobile menu', () => {
+    renderWithTheme(<Menu />)
+
+    // Seleciona o menu
+    const fullMenuElement = screen.getByRole('navigation', { hidden: true })
+
+    // Verifica se o menu está escondido como padrão
+    expect(fullMenuElement.getAttribute('aria-hidden')).toBe('true')
+    expect(fullMenuElement).toHaveStyle({ opacity: 0 })
+
+    // Verifica se o menu abre ao clicar no botão de abrir
+    fireEvent.click(screen.getByLabelText(/open menu/i))
+    expect(fullMenuElement.getAttribute('aria-hidden')).toBe('false')
+    expect(fullMenuElement).toHaveStyle({ opacity: 1 })
+
+    // Verifica se o menu fecha ao clicar no botão de fechar
+    fireEvent.click(screen.getByLabelText(/close menu/i))
+    expect(fullMenuElement.getAttribute('aria-hidden')).toBe('true')
+    expect(fullMenuElement).toHaveStyle({ opacity: 0 })
+  })
+
+  it('should show register box when logged out', () => {
+    renderWithTheme(<Menu />)
+
+    expect(screen.queryByText(/my account/i)).not.toBeInTheDocument()
+    expect(screen.queryByText(/wishlist/i)).not.toBeInTheDocument()
+    expect(screen.getByText(/log in now/i)).toBeInTheDocument()
+    expect(screen.getByText(/sign up/i)).toBeInTheDocument()
+  })
+
+  it('should show wishlist and account when logged in', () => {
+    renderWithTheme(<Menu userName="eduardo" />)
+
+    expect(screen.getByText(/my account/i)).toBeInTheDocument()
+    expect(screen.getByText(/wishlist/i)).toBeInTheDocument()
+    expect(screen.queryByText(/log in now/i)).not.toBeInTheDocument()
+    expect(screen.queryByText(/sign up/i)).not.toBeInTheDocument()
   })
 })
